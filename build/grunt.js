d7bbc50e6f5924580be17165f0b97ac661b912ab
LosBombz <carlos.b.escobar@gmail.com>
2013-01-27 21:10:25 -0500

    updated grunt file, setting up build

--- build/grunt.js
+++ build/grunt.js
@@ -1,58 +1,66 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: '<json:package.json>',
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-    },
-    lint: {
-      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    concat: {
-      dist: {
-        src: ['<banner:meta.banner>', '<file_strip_banner:lib/<%= pkg.name %>.js>'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    min: {
-      dist: {
-        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint qunit'
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true
-      },
-      globals: {}
-    },
-    uglify: {}
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: '<json:package.json>',
+        lint: {
+            files: ['<%= pkg.paths.src %>' + '<%= pkg:paths.js %>' + '**']
+        },
+        compass : {
+            dev : {
+                src: '<%= pkg.paths.src %>' + '<%= pkg.paths.sass %>',
+                dest: '<%= pkg.paths.src %>' + '<%= pkg.paths.css %>',
+                linecomments: true,
+                forcecompile: true,
+                debugsass: true,
+                images: '<%= pkg.paths.src %>' + '<%= pkg.paths.images %>',
+                relativeassets: true
+            },
+            prod : {
+                src: '<%= pkg.paths.src %>' + '<%= pkg.paths.sass %>',
+                dest: '<%= pkg.paths.dest %>' + '<%= pkg.paths.css %>',
+                outputstyle: 'compressed',
+                linecomments: false,
+                forcecompile: true,
+                debugsass: false,
+                images: '<%= pkg.paths.src %>' + '<%= pkg.paths.images %>',
+                relativeassets: true
 
-  // Default task.
-  grunt.registerTask('default', 'lint qunit concat min');
+            }
+        },
+        watch: {
+            files: ['<%= pkg.paths.src %>' + '<%= pkg.paths.sass %>' + '/*.scss'],
+            tasks: ['compass:dev']
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                browser: true
+            },
+            globals: {
+                '$' : true,
+                '_' : true,
+                'Backbone' : true,
+                'require' : true,
+                'define' : true,
+                'soundManager' : true,
+                'console' : true
+            }
+        }
+    });
+    grunt.loadNpmTasks( 'grunt-compass' );
+
+    // Default task.
+    grunt.registerTask('default', 'lint compass:prod');
 
 };
