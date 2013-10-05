cfa729dc725b459b95197dc8b1b8b087904c170c
Carlos Escobar <carlos.b.escobar@gmail.com>
2013-10-05 17:18:04 -0400

    added new tracks

--- src/scripts/tracks.js
+++ src/scripts/tracks.js
@@ -4,23 +4,23 @@ define(function(){
             {
 
                 'trackId': 'td-4-27-12',
-                'trackUrl' : '/tracks/friday-april-27-2012-td.mp3',
+                'trackUrl' : '/tracks/friday-april-27-2012-td-1.mp3',
                 'title' : 'Tommy doyles live set - 4/27/2012 ',
                 'artist' : 'DJ. Skitz',
                 'cover' : '/images/mixes/td.jpg',
                 'downloadUrl' : '/tracks/td-4-27-12.mp3',
                 'trackMarks' : [10, 20, 30, 40, 60,80]
-            },  
-            {   
+            },
+            {
                 'trackId': 'after-hours',
                 'title' : 'After Hours',
-                'trackUrl' : '/tracks/after-hours.mp3',
+                'trackUrl' : '/tracks/after-hours-1.mp3',
                 'artist' : 'DJ. Skitz',
                 'cover' : '/images/mixes/ah.png',
                 'downloadUrl' : '/tracks/AfterHours.mp3',
                 'trackMarks' : [10, 20, 30, 40, 60,80]
             },
-            {   
+            {
                 'trackId': 'good-good-night',
                 'title' : 'Good Good Night (DJ Skitz Blend)',
                 'trackUrl' : '/tracks/good-good-night.mp3',
@@ -28,7 +28,7 @@ define(function(){
                 'cover' : '/images/mixes/td.jpg',
                 'downloadUrl' : '/tracks/AfterHours.mp3',
                 'trackMarks' : [10, 20, 30, 40, 60,80]
-            }            
+            }
 
         ];
         
